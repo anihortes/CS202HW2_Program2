54338a191ada3ea28a97b92a4e39c215756b0b0b
Adam Owen <ahowenjr@alaska.edu>
2021-02-12 22:18:46 -0900

    final commit

--- main.cpp
+++ main.cpp
@@ -2,41 +2,34 @@
 using std::cout;
 using std::endl;
 static int stats = 0;
-struct Data{
-    int temp = 0;
-    int temp2 = 0;
-    int temp3 = 0;
-};
-void printPointer(int i){
-    int newint = 0;
-    int* iptr = &i;
+
+void printPointer(int stackPtrOne){
+    int stackPtrTwo = 0;
+    int* iptr = &stackPtrOne;
     //the unary * operator dereferecnes the pointer
-    cout << "i =" << *iptr << endl;
-    cout << "&i =" << iptr << endl;
-    cout << "j =" << newint << endl;
-    cout << "&j =" << &newint << endl;
+    cout << "&stackPtrOne =" << &iptr << endl;
+    cout << "&stackPtrTwo =" << &stackPtrTwo << endl;
 
 }
-int* outsidePtr;
-int* otherPtr;
+int* staticPtrOne;
+int* staticPtrTwo;
 int main() {
-    cout << "Demonstrates stack growing towards address 0x00000000" << endl;
-    int i =0;
-    printPointer(i);
-    cout << "j is allocated space on the stack after i and &j < &i" << endl;
-    int d;
-    Data temp;
-    int *ptr;
-    int *ptr2;
-    ptr = &temp.temp;
-    ptr2 = &temp.temp2;
-    outsidePtr = &stats;
-    cout << "ptr =" << *ptr << endl;
-    cout << "&ptr =" << &ptr << endl;
-    cout << "ptr2 =" << *ptr2 << endl;
-    cout << "&ptr2 =" << &ptr2 << endl;
-    cout << "outsidePtr =" << *outsidePtr << endl;
-    cout << "&outsidePtr =" << &outsidePtr << endl;
-    cout << "&otherPtr =" << &otherPtr << endl;
+    cout << "Demonstrates that the stack growing towards address 0x00000000." << endl;
+    int stackPtrOne =0;
+    printPointer(stackPtrOne);
+    cout << "stackPtrTwo is allocated space on the stack after stackPtrOne" << endl;
+
+    staticPtrOne = &stats;
+    cout << "Demonstrates where static data is stored." << endl;
+    cout << "&staticPtrOne =" << &staticPtrOne << endl;
+    cout << "&staticPtrTwo =" << &staticPtrTwo << endl;
+
+    int *freePtrOne = new int;
+    int *freePtrTwo = new int;
+    cout << "Demonstrates where free data is stored." << endl;
+    cout << "&freePtrOne =" << &freePtrOne << endl;
+    cout << "&freePtrTwo =" << &freePtrTwo << endl;
+    delete freePtrOne;
+    delete freePtrTwo;
     return 0;
 }
